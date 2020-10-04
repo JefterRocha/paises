ba50d2f286034d09441ab7621a69e6013dd38b04
JefterRocha <jefterrocha7@gmail.com>
2020-10-04 17:44:36 -0300

    building app

---
+++ site/precache-manifest.19d2bc62ed56a971ce764453860799d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e469fafeb405b84f5476bde9826ba8ec",
+    "url": "/paises/index.html"
+  },
+  {
+    "revision": "1b2d51f43ba8662d7c8b",
+    "url": "/paises/static/css/main.cde12f0e.chunk.css"
+  },
+  {
+    "revision": "c2992c9e6d190d0c8cfa",
+    "url": "/paises/static/js/2.ce5128f0.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/paises/static/js/2.ce5128f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2d51f43ba8662d7c8b",
+    "url": "/paises/static/js/main.98e6d555.chunk.js"
+  },
+  {
+    "revision": "3bb77c50f76b8e282f7f",
+    "url": "/paises/static/js/runtime-main.1b72b280.js"
+  }
+]);
